ae7648a8db9278c12faf927820ca885616f4fdcf
gilang aditya <madegilangaditya32@gmail.com>
2022-08-04 23:13:05 +0800

    add fetch ffrom json

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 console.log('masuk');
+const cardList = document.querySelector('#card-list');
+let html = '';
+fetch('../data.json').then(
+    (response) => response.json()
+).then(
+    (data) => (
+        data.forEach(datas => {
+            console.log(datas);
+            html += `
+            <div class="card ${datas.title.toLowerCase().replaceAll(' ', '-')}">
+                <div class="card-wrap">
+                <div class="card-wrap-title">
+                    <h6>${datas.title}</h6>
+                    <img src="./images/icon-ellipsis.svg" alt="elipsis">
+                </div>
+                <div class="card-wrap-time">
+                    <p class="time-title">${datas.timeframes.weekly.current}hrs</p>
+                    <p class="last-week">Last Week - ${datas.timeframes.weekly.previous}hrs</p>
+                </div>
+                </div>
+            </div>
+            `;
+            console.log(html);
+            cardList.innerHTML = html;
+            
+        })
+        
+    )
+);
+
